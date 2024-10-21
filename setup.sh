b73105624672bb89c949396954030da10de1b515
girish <vardhan572004@gmail.com>
2024-10-21 16:45:54 +0530

    Updated Setup and Requirements

--- setup.sh
+++ setup.sh
@@ -3,16 +3,18 @@
 # Create virtual environment
 python -m venv venv
 
-# Activate virtual environment (for Windows Git Bash / MinGW)
+#  Activate virtual environment (for Windows Git Bash / MinGW)
 source venv/Scripts/activate
 
 # Upgrade pip
-pip install --upgrade pip
+# pip install --upgrade pip
 
-# Install requirements
-pip install -r requirements.txt
 
 # Download spaCy model
 python -m spacy download en_core_web_sm
 
-echo "Setup complete. Run 'source venv/Scripts/activate' to activate the virtual environment."
+# Install requirements
+pip install -r requirements.txt
+
+
+echo -e "\nSetup complete. \nNow Run 'source venv/Scripts/activate' to activate the virtual environment."
